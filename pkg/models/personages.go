593d1692c531c519e14e684e79b23370cea457dc
cra4f <cra4fsergey@yandex.ru>
2024-08-06 09:17:16 +0300

    update personage model

--- pkg/models/personages.go
+++ pkg/models/personages.go
@@ -1,10 +1,10 @@
 package models
 
 type Personage struct {
-	Id          int
-	Name        string
-	Description string
-	EarnPercent float64
-	EarnPeriod  float64
-	LifePeriod  float64
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	EarnPercent float64 `json:"earn_percent"`
+	EarnPeriod  float64 `json:"earn_period"`
+	LifePeriod  float64 `json:"life_period"`
 }
